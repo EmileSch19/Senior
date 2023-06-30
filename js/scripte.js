d7a5efde1cc65c9a06514438b3e24ba18293fab8
Hashem195 <hashem.w.h@gmail.com>
2023-06-30 16:49:48 +0200

    Create scripte.js
    slideshow javascript

---
+++ js/scripte.js
@@ -0,0 +1,22 @@
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let currentSlide = 0;
+
+slides[currentSlide].classList.add('active');
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+function prevSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
