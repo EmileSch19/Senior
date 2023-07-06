622e0f5a14ca9e9ef81e09e47708562f37a84df9
EmileSch19 <emile.dufoulon@outlook.com>
2023-07-06 10:33:26 +0200

    Mofif

---
+++ js/Script.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('.input');
+const recherche= document.querySelector('.search');
+const fermer = document.querySelector('Button');
+
+
+// Action a mené
+
+recherche.addEventListener('click', () => {
+    input.style.display = 'block';
+  });
+  
+  fermer.addEventListener('click', () => {
+    input.style.display = 'none';
+  });
--- js/scripte.js
+++ js/scripte.js
@@ -1,9 +1,7 @@
 const slides = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
-const input = document.querySelector('.input');
-const recherche= document.querySelector('.search');
-const fermer = document.querySelector('Button');
+
 
 let currentSlide = 0;
 
@@ -21,14 +19,10 @@ function prevSlide() {
   slides[currentSlide].classList.add('active');
 }
 
+
+
 nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
-recherche.addEventListener('click', () => {
-  input.style.display = 'block';
-});
 
-fermer.addEventListener('click', () => {
-  input.style.display = 'none';
-});
 
  
